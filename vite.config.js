cc2532d9e17cfb6c3fe789aca90db5c341aa5118
xiaocol <450823280@qq.com>
2026-05-04 14:17:59 +0900

    Fix syntax error in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/BOMCompare/' 
+  base: '/BOMCompare/' ,
   plugins: [react]
 });
